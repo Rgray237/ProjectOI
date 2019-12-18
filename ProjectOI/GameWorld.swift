3a44a8dae1ef30b9f16ddc148dca0c079352f214
Robert Gray <rgray237@gmail.com>
2019-12-18 17:24:14 -0600

    Create GameWorld.swift

---
+++ ProjectOI/GameWorld.swift
@@ -0,0 +1,22 @@
+
+
+import Foundation
+
+
+class GameWorld
+{
+  var objects:[GameObject]
+  var player: Player!
+  var enemies: [Enemy]
+  var obstacles: [Obstacle]
+  var neurons: [Neuron]
+  var portals: [Portal]
+  var planes: [Plane]
+  
+  init(Level:Int)
+  {
+  
+  }
+  
+
+}
