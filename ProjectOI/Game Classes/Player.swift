07a1a48d0ac4e856a923deaeac6fc617bee4b7d6
Robert Gray <rgray237@gmail.com>
2020-01-13 18:55:31 -0600

    exploring state machine design patterns for player movement... WIP

--- ProjectOI/Game Classes/Player.swift
+++ ProjectOI/Game Classes/Player.swift
@@ -1,11 +1,85 @@
 import Foundation
 
 
+import Foundation
+
+class Input
+{
+
+}
+
+protocol  PlayerState
+{
+    static func handleInput(plyr:Player,in:Input)
+    static func update(plyr:Player)
+}
+
+class IdleState : PlayerState
+{
+   static func handleInput(plyr:Player,in:Input)
+    {
+    }
+
+   static func update(plyr:Player)
+    {
+    }
+}
+
+class DashingState : PlayerState
+{
+    static func handleInput(plyr:Player,in:Input)
+    {
+    }
+
+    static func update(plyr:Player)
+    {
+    }
+}
+
+class CooldownState : PlayerState
+{
+    static func handleInput(plyr:Player,in:Input)
+    {
+    }
+
+    static func update(plyr:Player)
+    {
+    }
+}
+
+
 
 class Player : SentientActor
 {
-    override init() {
-        super.init()
-        
+
+    internal var state:PlayerState
+
+    init() {
+        state = IdleState()
     }
+
+    func changeState()
+    {
+        state = DashingState()
+    }
+
+    func getState()
+    {
+        if state is IdleState
+        {
+            print("idle")
+        }
+
+        if state is DashingState
+        {
+            print("dashing")
+        }
+
+    }
+    
 }
+
+let plr = Player()
+plr.getState()
+plr.changeState()
+plr.getState()
